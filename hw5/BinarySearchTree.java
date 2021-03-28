5c19a1341290f76ba7bae37a7c0181fbe1d97bb3
Claire Jensen <clairej735@gmail.com>
2021-03-28 14:31:47 -0400

    more work

---
+++ hw5/BinarySearchTree.java
@@ -0,0 +1,26 @@
+public class BinarySearchTree<K extends Comparable<K>, V> {
+
+    public V add(K key, V value) {
+        if(value == this.lookup(key)) {
+
+        }
+    }
+
+    public V remove(K key) {
+        if(this.lookup(key) == null) {
+            return null;
+        } else {
+
+        }
+
+    }
+
+    public V lookup(K key) {
+        
+    }
+
+    public void inOrderTraverse() {
+
+    }
+
+}
