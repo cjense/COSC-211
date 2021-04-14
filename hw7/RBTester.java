c3892b193e7eee6843eab848ef9cee1296bed136
Claire Jensen <clairej735@gmail.com>
2021-04-14 16:39:51 -0400

    working

--- hw7/RBTester.java
+++ hw7/RBTester.java
@@ -55,13 +55,84 @@ public class RBTester {
 
     }
 
+    private static boolean inOrder(RedBlackTree<Integer> rbt, RBNode<Integer> node) {
+        RBNode<Integer> root = rbt.root;
+        node = root;
+
+        if (node.red && node.left.red != true && node.right.red != true) {
+            return false;
+        }
+
+        if(node.left != null && node.right != null) {
+            inOrder(rbt, node.left);
+            inOrder(rbt, node.right);
+        }
+
+        return true;
+
+    }
+
+    private static boolean rootChecker(RedBlackTree<Integer> rbt) {
+        RBNode<Integer> root = rbt.root;
+
+        if(root.red != false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean nullLeafChecker(RedBlackTree<Integer> rbt) {
+
+
+        return false;
+    }
+
+    private static boolean redChildren(RedBlackTree<Integer> rbt) {
+
+        RBNode<Integer> root = rbt.root;
+        RBNode<Integer> left = root.left;
+        RBNode<Integer> right = root.right;
+
+        // go through left side of tree to check for red nodes' children
+        while(left != null) {
+            if(left.red) {
+                if(left.right.red != true && left.left.red != true) {
+                    left = left.left;
+                } else { return false; }
+            } else {
+                left = left.left;
+            }
+        }
+
+        while(right != null) {
+
+        }
+
+        if(count == 2) {
+            return true;
+        } else {
+            return false;
+        }
+
+        return false;
+    }
+
+    private static boolean blackTraverse(RedBlackTree<Integer> rbt) {
+
+
+        return false;
+    }
+
 
     private static boolean isRBTree (RedBlackTree<Integer> rbt) {
 
-	RBNode<Integer> root = rbt.root;
+        if(rootChecker(rbt) && nullLeafChecker(rbt) && redChildren(rbt) && blackTraverse(rbt)) {
+            return true;
+        } else {
+            return false;
+        }
 
-        // COMPLETE THIS METHOD.  YOU MAY/SHOULD USE HELPER METHODS.
-	
     }
 
 
